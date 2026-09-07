56aa0792fdb4a755d94b296c97794ff2501067df
Karan Pratap Singh <24bee052@nith.ac.in>
2026-09-07 10:50:49 +0530

    Add Bubble Sort solution

---
+++ Sorting/Bubble_sort.py
@@ -0,0 +1,10 @@
+class Solution:
+    def bubbleSort(self,arr):
+        # code here
+        for i in range(len(arr)-2,-1,-1):
+            for j in range(0,i+1):
+                if arr[j] > arr[j+1]:
+                    arr[j],arr[j+1] = arr[j+1],arr[j]
+        return arr
+
+#for each run of the main for loop, the highest element bubbles up
